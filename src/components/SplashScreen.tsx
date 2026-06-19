9bc12dffbe041dd8ffe5b71550bf64732f96c506
ukstander-co <ukstander.co@gmail.com>
2026-06-19 18:00:41 +0500

    Update SplashScreen.tsx

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { motion } from 'motion/react';
+import { m as motion } from 'motion/react';
 import { ShoppingBag } from 'lucide-react';
 
 export default function SplashScreen({ onComplete }: { onComplete: () => void }) {
